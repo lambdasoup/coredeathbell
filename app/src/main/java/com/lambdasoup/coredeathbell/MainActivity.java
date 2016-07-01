e6c0dfc686b596db5f419b65e62a6d60e16178b7
Juliane Lehmann <jl@lambdasoup.com>
2016-07-01 22:59:08 +0200

    Adds About activity; fixes up colors and theme

--- app/src/main/java/com/lambdasoup/coredeathbell/MainActivity.java
+++ app/src/main/java/com/lambdasoup/coredeathbell/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -41,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     public void onButtonSettingsClicked(View view) {
         startActivity(new Intent(getApplicationContext(), SettingsActivity.class));

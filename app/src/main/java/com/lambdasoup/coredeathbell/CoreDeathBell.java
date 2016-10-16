9d67ce9a1c95c37d2bf0e34129ba34c0b655fb8e
Max <docoptix@amsf.de>
2016-10-16 17:37:13 +0200

    update to v2 (+ unlimited signal) topic

--- app/src/main/java/com/lambdasoup/coredeathbell/CoreDeathBell.java
+++ app/src/main/java/com/lambdasoup/coredeathbell/CoreDeathBell.java
@@ -4,15 +4,12 @@ import android.app.Application;
 
 import timber.log.Timber;
 
-/**
- * Created by jl on 01.07.16.
- */
 public class CoreDeathBell extends Application {
-    public static final String TOPIC_NON_CORE_BLOCK_MINED = "NonCoreBlockMined";
-    public static final int PENDING_INTENT_PLAY_SERVICES_NOT_AVAILABLE = 1;
-    public static final int NOTIFICATION_PLAY_SERVICES_NOT_AVAILABLE = 1;
-    public static final int PENDING_INTENT_NON_CORE_BLOCK_MINED = 0;
-    public static final int NOTIFICATION_NON_CORE_BLOCK_MINED = 0;
+    public static final String TOPIC_NON_CORE_BLOCK_MINED                 = "v2";
+    public static final int    PENDING_INTENT_PLAY_SERVICES_NOT_AVAILABLE = 1;
+    public static final int    NOTIFICATION_PLAY_SERVICES_NOT_AVAILABLE   = 1;
+    public static final int    PENDING_INTENT_NON_CORE_BLOCK_MINED        = 0;
+    public static final int    NOTIFICATION_NON_CORE_BLOCK_MINED          = 0;
 
     @Override
     public void onCreate() {
